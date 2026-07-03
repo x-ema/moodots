5c098634e727459b2979e0713a51a0a420d877f3
x-ema <alexmdb0+github@gmail.com>
2026-07-03 09:04:51 -0700

    rename hyprland.lua -> general.lua to be in line with what ii expects, add yazi config

---
+++ config/hypr/custom/general.lua
@@ -0,0 +1,114 @@
+-- Vars
+
+local terminal = "kitty"
+local browser = "firefox"
+local editor = "kitty -e nvim"
+local file_browser = "kitty -e yazi"
+
+-- Startup & Shutdown
+hl.on("hyprland.start", function()
+    hl.exec_cmd("systemctl --user start hyprland-session.target")
+end)
+
+
+hl.on("hyprland.shutdown", function()
+    os.execute("systemctl --user stop hyprland-session.target && sleep 0.1")
+    -- uses a blocking exec function and sleeps a bit to give things time to close
+    -- you might also want to kill troublesome/crashing non-systemd background services here:
+    -- os.execute("pkill wallpaperthing; systemctl --user stop hyprland-session.target && sleep 0.1")
+end)
+
+-- Layout Config
+
+hl.config({
+  general = {
+    layout = "scrolling"
+  },
+  decoration = {
+    rounding = 20,
+    active_opacity = 1,
+    inactive_opacity = 0.5,
+--    fullscreen_opaticy = 1,
+    blur = {
+      enabled = true,
+      new_optimizations = true
+    }
+--    motion_blur = {
+--      enabled = true,
+--      samples = 7
+--      Motion blur not working for now??? "unknown config key decoration.motion_blur.enabled samples etc"
+    }
+  },
+  input = {
+    touchpad = {
+      disable_while_typing = false,
+      natural_scroll = true, 
+      clickfinger_behavior = true
+    }
+  }
+})
+
+-- Unbinds
+
+local unbinds = {
+  "CTRL + SUPER + Left",
+  "CTRL + SUPER + Right",
+  "SUPER + Page_Down",
+  "SUPER + Page_Up",
+  "SUPER + F",
+  "SUPER + ALT + F",
+  "SUPER + D",
+  "SUPER + ALT + Space",
+  "SUPER + A",
+  "SUPER + ALT + A",
+  "SUPER + SUPER_L",
+  "SUPER + SUPER_R",
+  "SUPER + W",
+  "SUPER + RETURN",
+  "SUPER + code:19",
+  "SUPER + S",
+  "SUPER + H",
+  "SUPER + J",
+  "SUPER + K",
+  "SUPER + L",
+  "SUPER + mouse_right",
+  "SUPER + mouse_left",
+  "SUPER + T",
+  "SUPER + C",
+  "SUPER + E"
+
+}
+
+for i = 0,9 do 
+  table.insert(unbinds, "SUPER + "..i)
+  table.insert(unbinds, "SUPER + SHIFT + .."..i)
+  table.insert(unbinds, "SUPER + ALT + "..i)
+end
+for i = 10,19 do 
+  table.insert(unbinds, "SUPER + code:"..i)
+end
+
+for _,key in pairs(unbinds) do 
+  hl.unbind(key)
+end
+
+-- Binds
+hl.bind("SUPER + 1", hl.dsp.focus({ workspace = "r-1" }), { description = "Workspace: Focus Left" })
+hl.bind("SUPER + 2", hl.dsp.focus({ workspace = "r+1" }), { description = "Workspace: Focus Right" })
+hl.bind("SUPER + SHIFT + 1", hl.dsp.window.move({ workspace = "r-1", follow = true }, { description = "Window: Move to previous workspace" }))
+hl.bind("SUPER + SHIFT + 2", hl.dsp.window.move({ workspace = "r+1", follow = true }, { description = "Window: Move to next workspace" }))
+hl.bind("SUPER + T", hl.dsp.exec_cmd(terminal), { description = "Application: Terminal" })
+hl.bind("SUPER + F", hl.dsp.exec_cmd(browser), { description = "Application: Web Browser"})
+hl.bind("SUPER + C", hl.dsp.exec_cmd(editor), { description = "Application: Text Editor"})
+hl.bind("SUPER + E", hl.dsp.exec_cmd(file_browser), { description = "Application: File Browser"})
+hl.bind("ALT + RETURN", hl.dsp.window.fullscreen({ mode = "fullscreen", action = "toggle" }), { description = "Window: Toggle Fullscreen" })
+
+-- Vim like movement for focusing windows + moving
+local focusdir = { {"H","l"}, {"J","d"}, {"K","u"}, {"L","r"} }
+for _,dir in pairs(focusdir) do 
+  hl.bind("SUPER + "..dir[1], hl.dsp.focus({ direction = dir[2] }), { description = "Window: Focus '"..dir[2].."'" })
+  hl.bind("SUPER + SHIFT + "..dir[1], hl.dsp.window.move({ direction = dir[2] }), { description = "Window: Move '"..dir[2].."'" })
+end
+
+hl.bind("SUPER + mouse_left", hl.dsp.layout("move +100"), { description = "Layout: Scroll Left" })
+hl.bind("SUPER + mouse_right", hl.dsp.layout("move -100"), { description = "Layout: Scroll Right" })
