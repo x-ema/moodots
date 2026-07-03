25174bf7193b8c05741dcc58d0fe3dc88f758606
x-ema <alexmdb0+github@gmail.com>
2026-07-03 06:37:47 -0700

    Moving back to end-4's ii dots, its just so much easier sadge

--- config/hypr/scripts/screenshot.fish
+++
@@ -1,4 +0,0 @@
-#!/bin/fish 
-
-grimblast copysave area $HOME/Pictures/Screenshots/(date +%h-%m-%y_%H:%M:%S).png
-notify-send "Saved screenshot to $(echo $HOME)/Pictures/Screenshots/"
--- config/wayvr/theme/gui/assets/slimevr/pause.sh
+++
@@ -1,1 +0,0 @@
-echo '{"type":"action","name":"toggle_pause_tracking"}' | websocat ws://127.0.0.1:21110
